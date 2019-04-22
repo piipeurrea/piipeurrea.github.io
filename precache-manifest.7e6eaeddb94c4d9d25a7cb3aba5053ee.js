b5095de287fd3c127eaee91f00e8b7d3242f5cda
Felipe Urrea <piipeurrea@gmail.com>
2019-04-22 11:17:02 -0500

    proyecto

---
+++ precache-manifest.7e6eaeddb94c4d9d25a7cb3aba5053ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a6a8c154a4dc82c94c8f",
+    "url": "/static/js/main.89919f9f.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a6a8c154a4dc82c94c8f",
+    "url": "/static/css/main.4a7f5ea2.chunk.css"
+  },
+  {
+    "revision": "7e2de9dce31f3a8f832e6b4d5169ac9e",
+    "url": "/index.html"
+  }
+];
